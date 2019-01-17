bc3f2698b4a6778c286da57fb1cbb39615f5d429
nonissue <andy@nonissue.org>
2019-01-17 02:06:07 -0700

    er

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 set fish_greeting
 
+set -x FZF_LEGACY_KEYBINDINGS 0
+set -x FZF_DEFAULT_COMMAND 'git ls-tree -r --name-only HEAD 2> /dev/null; or fd --type f --hidden --follow --exclude .git 2> /dev/null'
+set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+
 set -x EDITOR nvim
 set -x GREP_COLOR "1;37;45"
 
