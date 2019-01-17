1b614618aacb5742df7e6d5b92979858ae4b6a2f
nonissue <andy@nonissue.org>
2019-01-17 14:26:51 -0700

    who knows anymore

--- fish/config.fish
+++ fish/config.fish
@@ -10,10 +10,19 @@ set -x EDITOR nvim
 set -x GREP_COLOR "1;37;45"
 
 # fisher init
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
+#if not functions -q fisher
+#    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+#    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+#    fish -c fisher
+# end
+
+set -g fisher_path {$HOME}/.dotfiles/fish/fisher
+
+set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+
+for file in $fisher_path/conf.d/*.fish
+    builtin source $file 2> /dev/null
 end
 
 function ..    ; cd .. ; end
