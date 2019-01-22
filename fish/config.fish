28bb27b81c24c169b2f51c5ee3c06ffb930c74fb
nonissue <andy@nonissue.org>
2019-01-21 21:27:35 -0700

    added new fish function to config

--- fish/config.fish
+++ fish/config.fish
@@ -20,6 +20,8 @@ function lookbusy ; cat /dev/urandom | hexdump -C | grep --color "ca fe" ; end
 function t        ; command tree -C $argv ; end
 function view     ; nvim -R $argv ; end
 
+function jjrf     ; source ~/.config/fish/config.fish ; end
+
 # fixes bug with iterm/fish_mode_prompt?
 function fish_mode_prompt; end
 # iterm
