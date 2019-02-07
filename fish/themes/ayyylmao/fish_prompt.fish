102135eaa3d236214ade3ae4e43549fd0c6400d8
Andrew Williams <andy@nonissue.org>
2019-02-06 18:11:49 -0700

    modified ayyylmao theme

--- fish/themes/ayyylmao/fish_prompt.fish
+++ fish/themes/ayyylmao/fish_prompt.fish
@@ -12,10 +12,8 @@ function __ayyylmao_colors -S -a color_scheme -d 'Define colors used by nonissue
 
   switch "$color_scheme"
     case 'light'
+      # local vars with approximate names to just aid in theming
       set -l blue_gray              314a70
-      # set -l greys                cccccc 999999 333333
-      set -l greys                  424242 616161 757575
-      
       set -l grey_med               857f77
       set -l grey_dark              61676C
       set -l dark_yellow            EBC562
@@ -23,18 +21,21 @@ function __ayyylmao_colors -S -a color_scheme -d 'Define colors used by nonissue
       set -l dust_black             07171B
       set -l dust_black_bright      6E767B
       set -l dust_grey              839496
-      set -x dust_blue 0E558C
+      set -x dust_blue              0E558C
       set -l dust_bright_blue       1587DF
-      set -l dust_bright_red EC5E01
-      set -x dust_bright_yellow FFD56D
-      set -x dust_red EB7000
-      set -x dust_redalt BC4B01
-      set -l dust_bright_magneta FF9446
-      set -l dust_green 3D8479
-      set -l dust_bright_green 52B2A3
+      set -l dust_bright_red        EC5E01
+      set -x dust_bright_yellow     FFD56D
+      set -x dust_red               EB7000
+      set -x dust_redalt            BC4B01
+      set -l dust_bright_magneta    FF9446
+      set -l dust_green             3D8479
+      set -l dust_bright_green      52B2A3
 
+      set -l greys                  424242 616161 757575
       set -l blues                  $dust_bright_blue $dust_blue $blue_gray
       
+      # the actual global variables that need to be set 
+      # by each theme
       set -x set_path             (set_color -o $dust_blue)
       set -x set_prompt           $blues
       set -x set_extra            (set_color -o $grey_med)
@@ -44,7 +45,6 @@ function __ayyylmao_colors -S -a color_scheme -d 'Define colors used by nonissue
       set -x set_ind_clean        (set_color -o $grey_med)
       set -x set_ind_dirty        (set_color -o $dark_orange)
       set -x set_ind_mod          (set_color -o $dark_yellow)
-
     case 'spacedust'
       # Based on spacedust terminal theme
       # Minor adjustments, not exact
@@ -137,52 +137,51 @@ function __ayyylmao_colors -S -a color_scheme -d 'Define colors used by nonissue
       set -x repo_bg_clean                  normal normal --bold
       
       set -x segment_exit                   $grey[3] $grey[1]
+    case 'old-colors'
+      # just keepign these while i finish changes.
+      set -l sel_bg 06496F
+      set -l line_num 244F61
+      set -l gutter_bg 0F2930
+      set -l brown 6E5346
+      set -l light_brown CA763C
+      set -l dark_yellow EBC562
+      set -l dark_orange E35B00
+      set -l dark_green 008080
+      set -l dark_blue 09699D
 
+      set -l teal 4CBF99
+      set -l puke_green 86B300
+      set -l dust_blue 0E558C
+      set -l dust_red EB7000
+      set -l dust_redalt BC4B01
+      set -l orange EC5E01
+
+      set -l light_grey DEE0E1
+      set -l med_grey ABB0B6
+      set -l dark_grey 61676C
+      set -l darkest_grey 090e13
+      set -l blue_gray 314a70
+      set -l greys 424242 616161 757575
+
+      # this is actually insane,
+      # some of these don't even set the color they say
+      # they do, because i'm dumb
+      set -l setdarkgrey (set_color -o $dark_grey)
+      set -l setdarkergrey (set_color -o 333)
+      set -l setdarkestgrey (set_color -o $darkest_grey)
+      set -l setdarkerblue (set_color -o $blue_gray)
+      set -l setorange (set_color -o $dark_orange)
+      set -l setyellow (set_color -o $dark_yellow)
+      set -l setred (set_color -o $dust_redalt)
+      set -l setblue (set_color -o 314a70)
+      set -l setgreen (set_color -o $puke_green)
+      set -l midgrey (set_color -o 857f77)
+
+      set -l indcolors $dark_grey $med_grey $dark_grey
   end
 end
 
 ##########################
-
-set -x sel_bg 06496F
-set -x line_num 244F61
-set -x gutter_bg 0F2930
-set -x brown 6E5346
-set -x light_brown CA763C
-set -x dark_yellow EBC562
-set -x dark_orange E35B00
-set -x dark_green 008080
-set -x dark_blue 09699D
-
-set -x teal 4CBF99
-set -x puke_green 86B300
-set -x dust_blue 0E558C
-set -x dust_red EB7000
-set -x dust_redalt BC4B01
-set -x orange EC5E01
-
-set -x light_grey DEE0E1
-set -x med_grey ABB0B6
-set -x dark_grey 61676C
-set -x darkest_grey 090e13
-set -x blue_gray 314a70
-set -x greys 424242 616161 757575
-
-# this is actually insane,
-# some of these don't even set the color they say
-# they do, because i'm dumb
-set -x setdarkgrey (set_color -o $dark_grey)
-set -x setdarkergrey (set_color -o 333)
-set -x setdarkestgrey (set_color -o $darkest_grey)
-set -x setdarkerblue (set_color -o $blue_gray)
-set -x setorange (set_color -o $dark_orange)
-set -x setyellow (set_color -o $dark_yellow)
-set -x setred (set_color -o $dust_redalt)
-set -x setblue (set_color -o 314a70)
-set -x setgreen (set_color -o $puke_green)
-set -x midgrey (set_color -o 857f77)
-
-set -x indcolors $dark_grey $med_grey $dark_grey
-
 
 set -x theme_color_scheme light
 __ayyylmao_colors light
@@ -205,16 +204,12 @@ end
 
 
 function show_path
-  # $set_prompt
-  # set_color $set_prompt
   echo -en $set_path(prompt_pwd)
   echo -n " "
 end
 
 
 function show_status -a last_status
-
-    set --local current_color "FFF"
 
     if [ $last_status -ne 0 ]
         set current_color red
@@ -229,8 +224,6 @@ function show_status -a last_status
 
     echo -n " "
     
-    set_color $current_color
-    
     set_color normal
 end
 
@@ -239,14 +232,12 @@ function fish_right_prompt
 
     set --local LIMBO /dev/null
     set --local git_status (git status --porcelain 2> $LIMBO)
-    set --local extra "$set_extra𝌆" # indicates no files are staged
+    set --local extra "$set_extra𝌆"
 
-    set --local dark_grey 555
-    set -l status_l "$set_status_l<" #setcolor for status left
-    set -l status_r "$set_status_r>" #setcolor for status right
+    set -l status_l "$set_status_l<" 
+    set -l status_r "$set_status_r>"
  
     if [ (_git_branch_name) ]
-      # set -l setdarkgrey (set_color -o $dark_grey) #setcolor for branch name
       set -l git_branch (_git_branch_name)
       set git_info " $git_branch"
     end
@@ -281,7 +272,6 @@ function fish_right_prompt
         end
     end
 
-    set_color 090e13 # setcolor for path
     show_path
 
     echo -n -s $git_info
@@ -292,7 +282,6 @@ end
 
 
 function show_virtualenv_name
-
     if set -q VIRTUAL_ENV
         echo -en "["(basename "$VIRTUAL_ENV")"] "
     end
