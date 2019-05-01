f4da5e59c6140ba53a67866131a2ef8385539941
aw <andy@nonissue.org>
2019-04-30 18:47:09 -0600

    updated fish prompt git icons

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -170,7 +170,7 @@ function fish_right_prompt
                 set extra "$set_ind_mod*" #setcolor for git indicator (dirty)✱✲
             end
         else if [ ~(_is_git_dirty) ]
-            set git_info "$extra [$set_bright_green$git_branch (set_color normal)]" # setcolor for git branch?
+            set git_info "$extra [$set_bright_green$git_branch]" # setcolor for git branch?
         end
     end
 
