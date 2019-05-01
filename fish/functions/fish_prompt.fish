6e6777e9242064633c2bddcccbb5d2cf926f1271
aw <andy@nonissue.org>
2019-04-30 18:16:50 -0600

    updated fish prompt git icons

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -69,14 +69,14 @@ function __ayyylmao_colors -S -a color_scheme -d 'Define colors used by nonissue
       set -l blues                  $dust_bright_blue $dust_med_blue $dust_blue 
       
       # theme current colour scheme vars
-      set -x set_path               (set_color -o $dust_green)
+      set -x set_path               (set_color -o $dust_blue)
       set -x set_prompt             $blues
       set -x set_extra              (set_color -o $grey_med)
       set -x set_status_l           (set_color -o $grey_dark)
       set -x set_status_r           (set_color -o $grey_dark)
-      set -x set_branch             (set_color -o $dust_bright_blue)
+      set -x set_branch             (set_color -o $dark_yellow)
       set -x set_ind_clean          (set_color -o $grey_med)
-      set -x set_ind_dirty          (set_color -o $dark_orange)
+      set -x set_ind_dirty          (set_color -o EEEEEE)
       set -x set_ind_mod            (set_color -o $dark_yellow)
       set -x set_white              (set_color -o EEEEEE)
       set -x set_bright_green       (set_color -o $dust_bright_green)
@@ -107,9 +107,9 @@ function fish_prompt
 end
 
 function show_path
-    echo -n "$set_status_l "
+    echo -n "$set_status_l  "
     echo -en $set_path(prompt_pwd)
-    echo -n "$set_status_l "
+    echo -n "$set_status_l"
 end
 
 function show_status -a last_status
@@ -134,7 +134,7 @@ function fish_right_prompt
 
     set --local LIMBO /dev/null
     set --local git_status (git status --porcelain 2> $LIMBO)
-    set --local extra "$set_bright_green ◆" #-- others ⧒ ⧑ ⧔ ⧕ ⧖⧗ (times with÷) ≍⫏⧇⦿⦸⦷⦵⧆⧈⊜≡≣∗∅=⊡⋐⨀*⤲
+    set --local extra "$set_bright_green" #-- others ⧒ ⧑ ⧔ ⧕ ⧖⧗ (times with÷) ≍⫏⧇⦿⦸⦷⦵⧆⧈⊜≡≣∗∅=⊡⋐⨀*⤲
 
     set -l status_l "$set_status_l<" 
     set -l status_r "$set_status_r>"
@@ -162,12 +162,11 @@ function fish_right_prompt
     # WE gots to do smething
     if [ (_git_branch_name) ]
         set -l git_branch (_git_branch_name)
-        # set git_info "$set_bright_green$git_branch"
         if [ (_is_git_dirty) ]
-            set git_info "$set_status_l $set_ind_dirty✲ $set_branch$git_branch$set_status_l" #setcolor for git indicator (dirty), git branch𝌆
+            set git_info "$set_status_l $set_ind_dirty$set_branch$git_branch$set_status_l" #setcolor for git indicator (dirty), git branch𝌆
             if not [ -z (echo "$git_status" | grep -e '^[MDA]') ]
                 # If there is new or deleted files, update status𝌡
-                set extra "$set_ind_mod✱" #setcolor for git indicator (dirty)
+                set extra "$set_ind_mod*" #setcolor for git indicator (dirty)✱✲
             end
         else if [ ~(_is_git_dirty) ]
             set git_info "$extra $set_bright_green$git_branch" # setcolor for git branch?
