0694f21f1dcdcd3121e425eaadc8b28d5637bdc4
nonissue <andy@nonissue.org>
2019-05-22 02:00:47 -0700

    readded encfs-find

---
+++ fish/functions/encfs-find.fish
@@ -0,0 +1,26 @@
+function encfs-find -a filename location
+    pushd .
+    if test -n "$filename"
+        set TARGET $filename
+    else 
+        echo "filename required!"
+        return
+    end
+
+    if test -n "$location"
+        cd "/mnt/media/$location"
+        # set -l location "/mnt/media/$location"
+        set TARGET $filename
+	# set pwd "/mnt/media/$location"
+    else
+    	# set pwd "/mnt/media/remote/movies"
+        cd "/mnt/media/remote"
+    end
+    echo $TARGET in (pwd)
+    # cd $pwd
+    # set maxdepth to 1?
+    find */ -name "$TARGET*" -exec echo \n{} \; -exec encfsctl encode --extpass="echo $ENCFS_PW" /mnt/media/.remote/whatson3/media/ {} \;
+    popd
+
+    
+end
