7178cf5be7d639b21ee937cf0e4d650c4eedcfd0
aw <andy@nonissue.org>
2019-06-17 03:09:03 -0600

    switched from peco to fzf for fuzzy finding

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
   bind \cr 'peco_select_history (commandline -b)'
-  bind \cf peco_cd # Bind for peco change directory to Ctrl+F
+  #bind \cf peco_cd # Bind for peco change directory to Ctrl+F
 end
 
 #fzf_key_bindings
