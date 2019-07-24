73fd3eaae5d67d66d37fdbc1939aa06ef7f62658
nonissue <andy@nonissue.org>
2019-07-23 20:31:47 -0700

    updated posix-source to ignore comments and blank lines

--- fish/functions/posix-source.fish
+++ fish/functions/posix-source.fish
@@ -1,6 +1,18 @@
 function posix-source
 	for i in (cat $argv)
-		set arr (echo $i |tr = \n)
-  		set -gx $arr[1] $arr[2]
+            # split line on "="
+	    set arr (echo $i |tr = \n)
+            # capture first char so we can check it
+            set -l firstchar (string sub --length 1 $arr[1]) 
+            # Make sure current line isn't empty
+            if [ (string length $arr[1]) != 0 ]
+                # if firstchar = "#" we skip it since it is a comment
+                if [ $firstchar != "#" ]
+                    # echo what we are doing
+                    echo "Setting:" $arr[1]"="$arr[2]
+                    # set variable
+                    set -gx $arr[1] $arr[2]
+                end
+            end 
 	end
 end
