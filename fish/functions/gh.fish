587498908ad8f3d8808fa8d8bbf5f28f7031f48f
nonissue <andy@nonissue.org>
2020-10-19 21:09:24 -0600

    update fish

--- fish/functions/gh.fish
+++ fish/functions/gh.fish
@@ -2,33 +2,40 @@
 # for some reason the original didn't work
 # the repo one liner is goddamn disgusting, but i don't know perl well enough to do better
 # [x] issue with repos with periods in the url (ie. github.com/nonissue/plainest.git)
-# NOTE: This is still broken in some cases, need to take another pass at the absolutely horrendus series of regexes I wrote
+# - [ ] Handle cases where branch isn't available on remote
 
 function gh
-  set url 'https://github.com'
-  set branch (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-  # set repo (git remote show -n origin | perl -lne 'print $1 if /Fetch URL:(.*)/' | perl -lne 'print $1 if /github.com\/(.*)/' | perl -lne '/(.*)\.git/ ? print $2 : print')
-  # Breaks on repos with a period in them like github.com/nonissue/nonissue.org
-  # EDIT: I think it's fixed!
-  set repo (git remote show -n origin | perl -lne 'print $1 if /Fetch URL:(.*)/' | perl -lne 'print $1 if /github.com[\/:](.*[^.git].)/')
-  # Build GitHub URL
-  set url "$url/$repo"
+    set url 'https://github.com'
+    set branch (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
+    # set repo (git remote show -n origin | perl -lne 'print $1 if /Fetch URL:(.*)/' | perl -lne 'print $1 if /github.com\/(.*)/' | perl -lne '/(.*)\.git/ ? print $2 : print')
+    # Breaks on repos with a period in them like github.com/nonissue/nonissue.org
+    # EDIT: I think it's fixed!
+    # EDIT: Okay, no really, I think it's fixed
+    # Test cases:
+    # git@github.com:nonissue/colesavage-ca.git
+    # git@github.com:nonissue/co-cms.git
+    # git@github.com:nonissue/hammerspoon
+    # https://github.com/davedelong/sfsymbols.git
+    # all pass
+    set repo (git remote show -n origin | perl -lne 'print $1 if /Fetch URL:(.*)/' | perl -lne 'print $1 if /github.com[\/:](.*[^.git])/')
+    # Build GitHub URL
+    set url "$url/$repo"
 
-  if test (count $argv) -gt 0
-    # Find file path relative to git root
-    pushd (dirname $argv[1])
-      set pwd (pwd)
-      set git_dir (git rev-parse --show-toplevel)
-      set file (echo $pwd | sed 's#'$git_dir'##')
-      set file $file'/'(basename $argv[1])
-      set file (echo $file | sed 's/:\([0-9]*\)$/#L\1/')
+    if test (count $argv) -gt 0
+        # Find file path relative to git root
+        pushd (dirname $argv[1])
+        set pwd (pwd)
+        set git_dir (git rev-parse --show-toplevel)
+        set file (echo $pwd | sed 's#'$git_dir'##')
+        set file $file'/'(basename $argv[1])
+        set file (echo $file | sed 's/:\([0-9]*\)$/#L\1/')
 
-      set url (echo "$url/blob/$branch/$file" | sed 's#//#/#g')
-    popd
-  else
-    set url "$url/tree/$branch"
-  end
-  echo $url
-  # Go go go
-  open $url
+        set url (echo "$url/blob/$branch/$file" | sed 's#//#/#g')
+        popd
+    else
+        set url "$url/tree/$branch"
+    end
+    echo $url
+    # Go go go
+    open $url
 end
