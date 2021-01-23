f3b14206dd0c07386dd706fd7e20f29f938f9597
nonissue <andy@nonissue.org>
2021-01-22 23:09:14 -0700

    dblchk: working i think

--- fish/functions/dblchk.fish
+++ fish/functions/dblchk.fish
@@ -85,16 +85,6 @@ function diff_paths
     end
 end
 
-# set res (find /mnt/media/local -type f -printf "%p\n" | string replace "local" "remote" | xargs -L 1 -d '\n' | while read -lt remotepath    if not test -e $remotepath        echo (set_color red)"FAIL: "(set_color yellow)$remotepath(set_color normal)    else        # echo "PASS: "$remotepath    endend | nl -b a -w3 -nrz -s.' ' | string collect)
-# echo $res
-# echo $res | count
-
-# could also do 
-# 1) get local file path
-# 2) replace `local` with `remote` in path
-# 3) check if new file exists? lol way easier
-
-
 function read_confirm --description 'Ask the user for confirmation' --argument prompt
     if test -z "$prompt"
         set prompt "Continue? [y/n]"
@@ -115,9 +105,9 @@ end
 # opts:
 # -v or --verbose : enables debug mode
 # -d or --directory : optional : accepts path ro dir : sets base dir to search
+# -d or --directory : optional : accepts path ro dir : sets base dir to search
 # for files
 # -t or --time : optional : accepts int (1-99) : sets age threshold for deleting files
-# -f or --force : runs command without any prompts
 function dblchk --description "Check a dir and all its files exist in a dir in a different file branch"
 
     # Files are in folder: (source)
@@ -132,20 +122,24 @@ function dblchk --description "Check a dir and all its files exist in a dir in a
     printf "\n%s\n\n  %s\n" $sep1 $timestamp | tee -a $logpath
 
     # config for parsing command line args
-    set -l options 'v/verbose' 's/source=' 'd/destination= t/time=!_validate_int'
-    argparse -n fdel $options -- $argv
+    set -l options 'v/verbose' 's/source=' 'd/destination=' 't/time=!_validate_int --min -99 --max 99'
+    argparse -n dblchk $options -- $argv
 
     if set -q _flag_s
         if test -d $_flag_s
             set source_dir $_flag_s
             cd $source_dir
         else
-            echo "invalid source directory specified"
+            echo "  Invalid source directory specified"
+            echo $_flag_s
             return 1
         end
     else
-        echo "You must specify a source directory"
-        return 1
+        echo "  No source dir specified, using default: " 
+        set source_dir '/mnt/media/local'
+        echo "  "$source_dir 
+        set _flag_s 'undefined'
+        # return 1
     end
 
     if set -q _flag_d
@@ -153,21 +147,30 @@ function dblchk --description "Check a dir and all its files exist in a dir in a
             set dest_dir $_flag_d
             cd $dest_dir
         else
-            echo "invalid destination directory specified"
+            echo "  Invalid destination directory specified"
             return 1
         end
     else
-        echo "You must specify a destination directory"
-        return 1
+
+        echo "  No destination dir specified, using default: "
+        set _flag_d 'undefined'
+        set dest_dir '/mnt/media/remote'
+        echo "  "$dest_dir
+
         # set _flag_d false
         # set base_dir '/mnt/media/local'
         # cd $base_dir
     end
 
-    set -l time 5
+    set -l time +5
 
     if set -q _flag_t
-        set time $_flag_t
+        if [ $_flag_t -gt 0 ]
+            set time +$_flag_t
+        else
+            set time $_flag_t
+        end
+        echo "  Looking for files with age "$time" day(s)"
     else
         set -e _flag_t
         set _flag_t "not set"
@@ -185,10 +188,34 @@ function dblchk --description "Check a dir and all its files exist in a dir in a
 
     # print debug mode if param passed
     if set -q _flag_v
-        printf "\n%s\n\n  Verbose Mode\n  * _flag_s: %s\n  * _flag_d: %s\n" $sep2 $_flag_s $_flag_d | tee -a $logpath
+        printf "\n%s\n\n  Verbose Mode\n  * _flag_s: %s\n  * _flag_d: %s\n  * _flag_t: %s\n" $sep2 $_flag_s $_flag_d $_flag_t | tee -a $logpath
     end
 
     # print description
+    set -x res (find /mnt/media/local -type f -mtime $time -printf "%p\n" | string replace "local" "remote" | xargs -L 1 -d '\n' | while read -lt remotepath
+        if not test -e $remotepath
+            printf (set_color red)"NOT SYNCED: "(set_color yellow)" %s"(set_color normal) $remotepath | string replace "remote" "local"
+        else 
+            # echo (set_color green)"SYNCED: "(set_color blue)(echo $remotepath | string replace "remote" "local")(set_color normal)
+        end
+    end | nl -bt -s.' ' | string collect)
+# | nl -bt -w5 -n rn -s.' '
+    # find /mnt/media/local -type f -mtime +$time -printf "%p\n" | string replace "local" "remote" | xargs -L 1 -d '\n' | while read -lt remotepath
+    #     if not test -e $remotepath
+    #         echo (set_color red)"NOT SYNCED: "(set_color yellow)(echo $remotepath | string replace "remote" "local")(set_color normal)
+    #     else 
+    #         echo (set_color green)"SYNCED: "(set_color blue)(echo $remotepath | string replace "remote" "local")(set_color normal)
+    #     end
+    # end | nl -b a -w3 -nrn -s.' ' | string collect
+    
+    if [ (printf "%s" $res | nl | count) -ge 1 ]
+        echo $res
+        echo "  "(printf "%s" $res | nl -bt | count ) "file(s) not synced!"
+    else
+        echo "  No unsynced files found"
+    end
 
+
+    printf "\n%s\n\n" $sep1
 end
 
