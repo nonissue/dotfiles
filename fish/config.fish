cf201ea464d335c323f16f60c07775d4fd6d90b9
nonissue <andy@nonissue.org>
2021-07-03 16:18:46 -0700

    fix linux fish paths

--- fish/config.fish
+++ fish/config.fish
@@ -80,7 +80,7 @@ end
 function fish_mode_prompt
 end
 
-# `brew doctor` was giving a warning about /usr/local/sbin not being found 
+# `brew doctor` was giving a warning about /usr/local/sbin not being found
 # in fish path, so i'm setting it here (19-05-09)
 # Seems to be working and not duplicating in basic testing
 
@@ -100,7 +100,7 @@ end
 # info: https://github.com/fish-shell/fish-shell/issues/5117
 switch (uname)
     case Darwin
-        # This isn't working for sourcing macports, hmmm. 
+        # This isn't working for sourcing macports, hmmm.
         # works if we add one segment at a time
         # Do they have to be separated by colon?
         # lol, yup that seems to have fixed it
@@ -117,7 +117,8 @@ switch (uname)
         set -x LS_COLORS 'rs=0:di=36:ln=32:mh=00:pi=33:so=33:do=33:bd=00:cd=00:or=05;36:mi=04;93:su=31:sg=31:ca=00:tw=36:ow=36:st=36:ex=031:*.tar=00:*.tgz=00:*.arc=00:*.arj=00:*.taz=00:*.lha=00:*.lz4=00:*.lzh=00:*.lzma=00:*.tlz=00:*.txz=00:*.tzo=00:*.t7z=00:*.zip=00:*.z=00:*.dz=00:*.gz=00:*.lrz=00:*.lz=00:*.lzo=00:*.xz=00:*.zst=00:*.tzst=00:*.bz2=00:*.bz=00:*.tbz=00:*.tbz2=00:*.tz=00:*.deb=00:*.rpm=00:*.jar=00:*.war=00:*.ear=00:*.sar=00:*.rar=00:*.alz=00:*.ace=00:*.zoo=00:*.cpio=00:*.7z=00:*.rz=00:*.cab=00:*.wim=00:*.swm=00:*.dwm=00:*.esd=00:*.jpg=00:*.jpeg=00:*.mjpg=00:*.mjpeg=00:*.gif=00:*.bmp=00:*.pbm=00:*.pgm=00:*.ppm=00:*.tga=00:*.xbm=00:*.xpm=00:*.tif=00:*.tiff=00:*.png=00:*.svg=00:*.svgz=00:*.mng=00:*.pcx=00:*.mov=00:*.mpg=00:*.mpeg=00:*.m2v=00:*.mkv=00:*.webm=00:*.ogm=00:*.mp4=00:*.m4v=00:*.mp4v=00:*.vob=00:*.qt=00:*.nuv=00:*.wmv=00:*.asf=00:*.rm=00:*.rmvb=00:*.flc=00:*.avi=00:*.fli=00:*.flv=00:*.gl=00:*.dl=00:*.xcf=00:*.xwd=00:*.yuv=00:*.cgm=00:*.emf=00:*.ogv=00:*.ogx=00:*.aac=00:*.au=00:*.flac=00:*.m4a=00:*.mid=00:*.midi=00:*.mka=00:*.mp3=00:*.mpc=00:*.ogg=00:*.ra=00:*.wav=00:*.oga=00:*.opus=00:*.spx=00:*.xspf=00:;'navi widget fish | source
     case Linux
         set -g os Linux
-        set -U fish_user_paths "/opt/local/bin /opt/local/sbin /usr/local/opt/fzf/bin /usr/local/bin /usr/bin /usr/local/sbin /bin /usr/sbin /sbin /usr/local/opt/curl/bin /opt/local/bin /usr/local/bin"
+        # set -U fish_user_paths "/home/ops/.local/bin /opt/local/bin /opt/local/sbin /usr/local/opt/fzf/bin /usr/local/bin /usr/bin /usr/local/sbin /bin /usr/sbin /sbin /usr/local/opt/curl/bin /opt/local/bin /usr/local/bin"
+        fish_add_path "/home/ops/.local/bin /opt/local/bin /opt/local/sbin /usr/local/opt/fzf/bin /usr/local/bin /usr/bin /usr/local/sbin /bin /usr/sbin /sbin /usr/local/opt/curl/bin /opt/local/bin /usr/local/bin"
         # set -U fish_user_paths "~/.local/bin:/opt/local/bin:/opt/local/sbin:/usr/local/opt/fzf/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/bin:/usr/sbin:/sbin:/usr/local/opt/curl/bin:/opt/local/bin:/usr/local/bin"
         # set -g -x PATH $PATH
         # Alias FD for FZF
