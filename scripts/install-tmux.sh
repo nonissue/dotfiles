998c7cd5654508029196f6b78da19d8dcb526ae8
Andy Williams <andy@nonissue.org>
2021-11-28 20:23:33 -0700

    tweak tmux install script

--- scripts/install-tmux.sh
+++ scripts/install-tmux.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 sudo apt install -y build-essential autoconf automake pkg-config libevent-dev libncurses5-dev bison byacc
-git clone https://github.com/tmux/tmux.git ~/tmp/tmux; cd ~/tmp/tmux; ./autogen.sh; ./configure && make; sudo make install; tmux kill-server; tmux -V; rm -rf ~/tmp/tmux;
+git clone https://github.com/tmux/tmux.git ~/tmp/tmux; 
+cd ~/tmp/tmux; 
+./autogen.sh; ./configure && make; 
+sudo make install; tmux kill-server; 
+tmux -V; 
+# RM doesn't work fully i don't think?
+# rm -rf ~/tmp/tmux;
 # TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
