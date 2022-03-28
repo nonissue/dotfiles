b6189627de8b26641190b5e26d20977ab870320e
nonissue <andy@nonissue.org>
2022-03-27 18:33:48 -0700

    add install btop

---
+++ scripts/install-btop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# NOTE: Only for linux at the moment
+
+cd ~/tmp
+
+GITHUB_RESULT=$(curl -s https://api.github.com/repos/aristocratos/btop/releases/latest \
+  | grep x86_64 \
+  | grep musl \
+  | grep linux \
+  | cut -d '"' -f 4 \
+  | xargs echo
+)
+
+RELEASE_FILE=$(echo $GITHUB_RESULT | cut -d ' ' -f1)
+RELEASE_URL=$(echo $GITHUB_RESULT | cut -d ' ' -f2)
+
+curl -LO $RELEASE_URL
+mkdir ~/tmp/btop
+tar -xvjf $RELEASE_FILE -C ~/tmp/btop
+cd ~/tmp/btop
+sudo make install
+sudo make setuid
+
+rm -rf ~/tmp/$RELEASE_FILE
+rm -rf ~/tmp/btop
+
+# echo $RELEASE_FILE $RELEASE_URL
