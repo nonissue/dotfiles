31bb316cecce2287e9bfac10f46ff6ba1709b733
nonissue <andy@nonissue.org>
2023-05-06 21:51:46 -0600

    add exa install script

---
+++ scripts/install-exa.sh
@@ -0,0 +1,31 @@
+
+
+#!/bin/bash
+
+# NOTE: Only for linux at the moment
+
+cd ~/tmp
+
+
+GITHUB_RESULT=$(curl -s https://api.github.com/repos/ogham/exa/releases/latest \
+  | grep x86_64 \
+  | grep musl \
+  | grep linux \
+  | cut -d '"' -f 4 \
+  | xargs echo
+)
+
+RELEASE_FILE=$(echo $GITHUB_RESULT | cut -d ' ' -f1)
+RELEASE_URL=$(echo $GITHUB_RESULT | cut -d ' ' -f2)
+
+curl -LO $RELEASE_URL
+# mkdir ~/tmp/btop
+tar -xvjf $RELEASE_FILE -C ~/tmp
+cd ~/tmp/exa
+# sudo make install
+# sudo make setuid
+
+# rm -rf ~/tmp/$RELEASE_FILE
+# rm -rf ~/tmp/btop
+
+# echo $RELEASE_FILE $RELEASE_URL
