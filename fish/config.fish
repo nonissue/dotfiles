e29a4523478dfb39b7015bf8868734ec11a9a065
nonissue <andy@nonissue.org>
2025-09-19 21:02:30 -0600

    add orbstack

--- fish/config.fish
+++ fish/config.fish
@@ -215,3 +215,7 @@ end
 # pnpm end
 
 alias claude="/Users/apw/.claude/local/claude"
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
